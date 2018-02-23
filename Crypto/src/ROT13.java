93e8d84fccdfe77eed6af7756708c2393542b353
Peter McCormick <petermccormick@zipcoders-MacBook-Pro-109.local>
2018-02-23 15:28:37 -0500

    ROT13 complete

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -12,21 +12,44 @@ public class ROT13  {
 
 
     public String crypt(String text) throws UnsupportedOperationException {
+        StringBuilder textSB = new StringBuilder();
+        for (int i = 0; i < text.length(); i++ ) {
+            char c = text.charAt(i);
 
-        return "";
+            if (c >= 'a' && c <= 'm') textSB.append(c += 13);
+
+            else if (c >= 'n' && c <= 'z') textSB.append(c -= 13);
+
+            else if (c >= 'A' && c <= 'M') textSB.append(c += 13);
+
+            else if (c >= 'N' && c <= 'Z') textSB.append(c -= 13);
+
+            else textSB.append(c);
+        }
+        return textSB.toString();
     }
 
     public String encrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
 
-        return "";
+        int startingIndex = 0;
+        StringBuilder textSB = new StringBuilder();
+        startingIndex = s.indexOf(c);
+
+        for (int i = startingIndex; i < s.length(); i++) {
+            textSB.append(s.charAt(i));
+        }
+        for (int i = 0; i < startingIndex; i++) {
+            textSB.append(s.charAt(i));
+        }
+        return textSB.toString();
     }
 
 }
